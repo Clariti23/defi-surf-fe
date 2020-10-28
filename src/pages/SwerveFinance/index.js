a1d354bb929a7f5b8d213b8acb82c8b8eef598b5
clariti23 <g.ferowich@gmail.com>
2020-10-28 17:21:00 -0500

    delete superfluous lines

--- src/pages/SwerveFinance/index.js
+++ src/pages/SwerveFinance/index.js
@@ -53,13 +53,6 @@ const SwerveFinance = (props) => {
                 { title: "24h Volume", iconClass: "bx-purchase-tag-alt", description: formatterEvilTwin.format(volume) }
             ];
 
-
-         const email = [
-                { title: "Week", linkto: "#", isActive: false },
-                { title: "Month", linkto: "#", isActive: false },
-                { title: "Year", linkto: "#", isActive: true }
-            ];
-
           return (
               <React.Fragment>
                 <div className="page-content">
@@ -101,113 +94,12 @@ const SwerveFinance = (props) => {
                                         </CardTitle>
                                     <TVL />
                                 </Card>
-                                    
-                                
-                                <Card>
-                                    <CardBody>
-
-                                        <CardTitle className="mb-4 float-sm-left">
-                                            Email Sent
-                                        </CardTitle>
-                                        <div className="float-sm-right">
-                                            <ul className="nav nav-pills">
-                                                {
-                                                    email.map((mail, key) =>
-                                                        <li className="nav-item" key={"_li_" + key}>
-                                                            <Link className={mail.isActive ? "nav-link active" : "nav-link"} to={mail.linkto}>{mail.title}</Link>
-                                                        </li>
-                                                    )
-                                                }
-                                            </ul>
-                                        </div>
-                                        <div className="clearfix"></div>
-                                        <StackedColumnChart />
-                                    </CardBody>
-                                </Card>
                             </Col>
                         </Row>
-
-                        
-
-                       
                     </Container>
                 </div>
-                <Modal isOpen={modal} role="dialog" autoFocus={true} centered={true} className="exampleModal" tabindex="-1" toggle={() => { setmodal(!modal); }}>
-                    <div className="modal-content">
-                        <ModalHeader toggle={() => { setmodal(!modal); }}>
-                            Order Details
-                        </ModalHeader >
-                        <ModalBody>
-                            <p className="mb-2">Product id: <span className="text-primary">#SK2540</span></p>
-                            <p className="mb-4">Billing Name: <span className="text-primary">Neal Matthews</span></p>
-
-                            <div className="table-responsive">
-                                <Table className="table table-centered table-nowrap">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Product</th>
-                                            <th scope="col">Product Name</th>
-                                            <th scope="col">Price</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">
-                                                <div>
-                                                    <img src={modalimage1} alt="" className="avatar-sm" />
-                                                </div>
-                                            </th>
-                                            <td>
-                                                <div>
-                                                    <h5 className="text-truncate font-size-14">Wireless Headphone (Black)</h5>
-                                                    <p className="text-muted mb-0">$ 225 x 1</p>
-                                                </div>
-                                            </td>
-                                            <td>$ 255</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                <div>
-                                                    <img src={modalimage2} alt="" className="avatar-sm" />
-                                                </div>
-                                            </th>
-                                            <td>
-                                                <div>
-                                                    <h5 className="text-truncate font-size-14">Hoodie (Blue)</h5>
-                                                    <p className="text-muted mb-0">$ 145 x 1</p>
-                                                </div>
-                                            </td>
-                                            <td>$ 145</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2">
-                                                <h6 className="m-0 text-right">Sub Total:</h6>
-                                            </td>
-                                            <td>$ 400</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2">
-                                                <h6 className="m-0 text-right">Shipping:</h6>
-                                            </td>
-                                            <td>Free</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2">
-                                                <h6 className="m-0 text-right">Total:</h6>
-                                            </td>
-                                            <td>$ 400</td>
-                                        </tr>
-                                    </tbody>
-                                </Table>
-                            </div>
-                        </ModalBody>
-                        <ModalFooter>
-                            <Button type="button" color="secondary" onClick={() => { setmodal(!modal); }}>Close</Button>
-                        </ModalFooter>
-                    </div>
-                </Modal>
             </React.Fragment>
-          );
-        }
+        );
+}
 
 export default withNamespaces()(SwerveFinance);
